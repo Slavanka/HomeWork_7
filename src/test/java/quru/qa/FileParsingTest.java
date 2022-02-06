6f448f237d07b880801f6a20ebfdd560410a9aa6
viktoria.klisheva <vikmah2@gmail.com>
2022-02-06 17:44:23 +0300

    .

---
+++ src/test/java/quru/qa/FileParsingTest.java
@@ -0,0 +1,47 @@
+package quru.qa;
+
+
+import com.codeborne.pdftest.PDF;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.xlstest.XLS;
+import com.opencsv.CSVReader;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.open;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class FileParsingTest {
+    private ClassLoader cl = FileParsingTest.class.getClassLoader();
+
+    @Test
+    void parsePdfTest() throws Exception{
+        open("https://litportal.ru/avtory/sergey-mihalkov/kniga-tri-porosenka-skazki-777008.html");
+        File pdfDownload = Selenide.$(byText("Скачать pdf")).download();
+        PDF parsed = new PDF(pdfDownload);
+        assertThat(parsed.text).contains("ТРИ ПОРОСЁНКА. СКАЗКИ");
+    }
+
+    @Test
+    void parseXlsTest() throws Exception{
+        try (InputStream stream = cl.getResourceAsStream("src/test/resource/files/svodnaya_tablica.xlsx")) {
+            XLS parsed = new XLS(stream);
+            System.out.println("");
+            assertThat(parsed.excel.getSheetAt(0).getRow(6).getCell(1).getStringCellValue())
+                    .isEqualTo("Картофель");
+        }
+    }
+
+    @Test
+    void parseCsvTest() throws Exception{
+        try (InputStream stream = cl.getResourceAsStream("src/test/resource/files/example.csv")) {
+            CSVReader reader = new CSVReader(new InputStreamReader(stream));
+
+
+        }
+    }
+}
